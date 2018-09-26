315a9d1c6d1181032c38f950b0dd44691da00360
joaoDragado <dubmecoco@yahoo.com>
2018-09-26 12:21:49 +0300

    more matrix operations

---
+++ intro_code_examples/26_daysMonth.c
@@ -0,0 +1,98 @@
+﻿/*****************************************************************************
+ * ΠΛΗ10 
+ * Εισαγωγή στη γλώσσα προγραμματισμού C.
+ * 
+ * 26o Αρχείο: 26_daysMonth.c
+ *
+ * Αυτό είναι το εικοστό έκτο πρόγραμμα προς επίδειξη.
+ * Πρόβλημα: Υπολογισμός των ημερών που έχει ένας μήνας, 
+ * πριν προλάβει ο χρήστης να πληκτρολογήσει όλους τους χαρακτήρες που αποτελούν το όνομά του. 
+ * Λύση: Καθώς ο χρήστης ξεκινάει να εισάγει το όνομα ενός μήνα, 
+ * ελέγχουμε έναν-έναν τους χαρακτήρες και παρουσιάζουμε το πλήθος 
+ * των ημερών του μήνα, αμέσως μόλις αυτό γίνει διακριτό. 
+ * Έννοιες που συναντάμε: Έλεγχος πολλαπλής συνθήκης, συναρτήσεις getch() και putchar().
+ ****************************************************************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    system("chcp 1253>nul");
+    int yr,days=0;
+    char ch;
+    printf("\nΔώστε το όνομα ενός μήνα (* για τέλος): ");
+    while( (ch = getch() ) != '*' )
+   {
+    putchar(ch);
+    switch (ch)
+    {
+    case 'σ':
+    case 'Σ':
+    case 'ν':
+    case 'Ν': days=30;
+	      break;
+    case 'μ':
+    case 'Μ':
+    case 'ο':
+    case 'Ο':
+    case 'δ':
+    case 'Δ': days=31;
+	      break;
+    case 'α':
+    case 'Α': ch=getch(); putchar(ch);
+	      switch (ch)
+	      {
+	      case 'υ':
+          case 'ύ':
+	      case 'Υ': days=31;
+			break;
+	      case 'π':
+	      case 'Π': days=30;
+			break;
+	      
+	      }
+	      break;
+    case 'ι':
+    case 'Ι': ch=getch(); putchar(ch);
+	      switch (ch)
+	      {
+	      case 'α':
+	      case 'Α': days=31;
+			break;
+	      case 'ο':
+	      case 'Ο': ch=getch(); putchar(ch); 
+            ch=getch(); putchar(ch);
+			switch (ch)
+			{
+			case 'ν':
+			case 'Ν': days=30;
+				  break;
+			case 'λ':
+			case 'Λ': days=31;
+				  break;
+		
+			}
+			break;
+	     
+	       }
+	       break;
+    case 'φ':
+    case 'Φ': printf("\nΔώστε τη χρονιά: ");
+	      scanf("%d",&yr);
+	      if (yr % 4 == 0 && yr % 100 != 0)    
+		days=29;
+	      else if (yr % 400 == 0)
+		days=28;
+	      break;
+   
+    }
+    if (days != 0) printf("\nέχει %d μέρες\n",days);
+    else
+      printf("\nΔεν υπάρχει τέτοιος μήνας\n");
+    days=0;
+    printf("\nΔώστε το όνομα ενός μήνα (* για τέλος):");
+   }
+   printf("\n");
+   system("pause");
+}
---
+++ intro_code_examples/28_structure.c
@@ -0,0 +1,110 @@
+﻿/*****************************************************************************
+ * ΠΛΗ10 
+ * Εισαγωγή στη γλώσσα προγραμματισμού C.
+ * 
+ * 28o Αρχείο: 28_domi.c
+ *
+ * Αυτό είναι το εικοστό όγδοο πρόγραμμα προς επίδειξη.
+ * Πρόβλημα: Να γράψετε τις αναγκαίες δηλώσεις για έναν πίνακα 50 θέσεων
+ * του οποίου τα στοιχεία είναι εγγραφές (structs) με πεδία name, age,
+ * salary. Στα πεδία αυτά θέλουμε να καταχωρούμε το όνομα, την ηλικία και
+ * το μισθό αντίστοιχα, κάποιου ατόμου.
+ * Λύση: Δημιουργούμε την struct και έπειτα με τις αντίστοιχες εντολές
+ * πραγματοποιούνται οι εισαγωγές νέων εγγραφών και η εκτύπωση - εμφάνισή τους.
+ * Έννοιες που συναντάμε: Δομή, εκτύπωση, προσθήκη εγγραφής
+ ****************************************************************************/
+ 
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 50
+
+typedef struct employee_record
+{
+    char name[20];
+    int age;
+    int salary;
+} Employee_Record;
+
+
+typedef Employee_Record Record_table[MAX_SIZE];
+
+int AddEntry(Record_table personnel, int tot)
+{
+    char temp;
+    if (tot > MAX_SIZE) // αν δεν υπάρχει αρκέτος χώρος στον πίνακα
+    {
+        return 0;
+    }
+    else
+    {
+        printf("Please enter name: ");
+        scanf("%s",personnel[tot].name);
+        printf("Please enter age: ");
+        scanf("%d",&personnel[tot].age);
+        printf("Please enter salary: ");
+        
+        do{
+            scanf("%c",&temp);
+        }while(temp != '\n');
+        
+        scanf("%d",&personnel[tot].salary);
+        return 1;
+    }
+}
+
+void PrintEntry(Employee_Record person)
+{
+    printf("Name:\t\t%s\n",person.name);
+    printf("Age:\t\t%d\n",person.age);
+    printf("Salary:\t\t%d\n",person.salary);
+}
+
+void PrintTable(Record_table personnel, int tot)
+{
+    int i;
+    for(i=0;i<tot;i++)
+    PrintEntry(personnel[i]);
+}
+
+int main()
+{
+    Record_table test;
+    Employee_Record TEST;
+    printf("Dwse ta stoixeia enos anthrwpou: \n");
+    AddEntry(test, 0); 
+    printf("\nEdwses ta parakatw stoixeia: \n");
+    PrintEntry(test[0]);
+    printf("Twra dwse ta stoixeia kai kapoiou allou!\n");
+    AddEntry(test, 1);
+
+    PrintTable(test , 2);
+    system("pause");
+    return 0;
+}
+///INPUT:
+//JOHN
+//18
+//1337
+
+///OUTPUT
+//Edwses ta parakatw stoixeia:
+//JOHN
+//18
+//1337
+//Twra dwse ta stoixeia kai kapoiou allou!
+
+///INPUT
+//Jace
+//21
+//2
+
+///OUTPUT
+//JOHN
+//18
+//1337
+//Jace
+//21
+//2
