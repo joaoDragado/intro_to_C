205488490c86b5b49bf4d0225d3e84527c06afab
joaoDragado <dubmecoco@yahoo.com>
2018-10-13 21:01:55 +0300

    circular linked list example added.

---
+++ intro_code_examples/alias.h
@@ -0,0 +1,13 @@
+#define PROGRAMMA main()
+#define ARXH   {
+#define TELOS  }
+#define DIAVASE  scanf
+#define EMFANISE printf
+#define AKERAIOI int
+#define EAN if
+#define ALLIOS else
+#define EPANALAVE do
+#define MEXRI while
+#define SYSTHMA system
+
+
