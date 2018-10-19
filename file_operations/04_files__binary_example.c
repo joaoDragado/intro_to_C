73a7f9319b2f136f24f620eac8cd18b163747e16
joaoDragado <dubmecoco@yahoo.com>
2018-10-19 18:10:11 +0300

    random access example added.

---
+++ file_operations/04_files__binary_example.c
@@ -0,0 +1,38 @@
+﻿/*
+Αρχείο: 04_arxeia_dyadika_example.c
+Πρόβλημα: Παράδειγμα αποθήκευσης στοιχείων σε δυαδικά αρχεία. Τυχαία προσπέλαση αρχείων
+Λύση: Η fwrite() γράφει έναν αριθμό από τμήματα συγκεκριμένου αριθμού byte σε μια περιοχή προσωρινής αποθήκευσης (buffer) και τα αποθηκεύει σε ένα αρχείο.
+Σύνταξη: int fwrite(void *buffer, int no_of_bytes, plithos, FILE *fp)
+no_of_bytes τον αριθμό bytes του κάθε τμήματος
+plithos: το πλήθος των τμημάτων
+*fp: δείκτης σε αρχείο
+Έννοιες που συναντάμε: fwrite()
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *arxeioA;
+	FILE *arxeioB;
+	FILE *arxeioC;
+	char seira1[20];
+	int seira2[5];
+	int akeraios = 33;
+
+    // Άνοιγμα αρχείων
+	arxeioA = fopen("arxeio1.bin", "w");
+	arxeioB = fopen("arxeio2.bin", "w");
+	arxeioC = fopen("arxeio3.bin", "w");
+
+
+	if (!fwrite(seira1, (sizeof(char)*20), 1, arxeioA))    // Εγγραφή δεδομένων στο arxeioA
+		printf(" Error seira1\n");
+	if (!fwrite(seira2, (sizeof(int)*5), 1, arxeioB))      // Εγγραφή δεδομένων στο arxeioB
+		printf(" Error seira2\n");
+	if (!fwrite(&akeraios, sizeof(akeraios), 1, arxeioC))  // Εγγραφή δεδομένων στο arxeioC
+		printf(" Error akeraios\n");
+		system("pause");
+}
---
+++ file_operations/05_files__binary_RadAcces.c
@@ -0,0 +1,57 @@
+﻿/* 
+Αρχείο: 05_arxeia_dyadika_RadAcces.c
+Πρόβλημα: Προσπέλαση δεδομένων σε δυαδικά αρχεία. Να εντοπιστούν και να τυπωθούν οι ζυγές εγγραφές αρχίζοντας από το τέλος
+Λύση: Η fseek() τοποθετεί το δείκτη του αρχείου σε κάποιο σημείο του αρχείου.
+Σύνταξη: int fseek(FILE *fp, long int apostasi, int thesi)
+*fp: δείκτης σε αρχείο
+Η παράμετρος apostasy δηλώνει την απόσταση σε byte από τη θέση που καθορίζει η παράμετρος thesi. Παράδειγμα:
+fp = fopen("arxeio", "rb");
+fseek(fp,100,0);
+Η fseek τοποθετεί το δείκτη θέσης του αρχείου στο 100 byte
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int int_size = sizeof(int);
+	FILE *arxeio;
+
+	int seira_akeraion[10];
+	int j,temp_akeraios;
+
+	arxeio = fopen("arxeio.txt", "w+");
+
+	seira_akeraion[0] = 10;
+	seira_akeraion[1] = 22;
+	seira_akeraion[2] = 25;
+	seira_akeraion[3] = 43;
+	seira_akeraion[4] = 54;
+	seira_akeraion[5] = 56;
+	seira_akeraion[6] = 67;
+	seira_akeraion[7] = 76;
+	seira_akeraion[8] = 77;
+	seira_akeraion[9] = 99;
+
+	fwrite(seira_akeraion, (int_size*10), 1, arxeio);
+
+	fclose(arxeio);
+
+	arxeio = fopen("arxeio.txt", "r");
+
+	fseek(arxeio, -int_size, SEEK_END);
+
+	printf("Oi epithimites egrafes tou arxeiou \"arxeio.txt\" einai:");
+	for(j=0;j<5;j++)
+	{
+		if(fread(&temp_akeraios, sizeof(temp_akeraios), 1, arxeio))
+			printf("%d ", temp_akeraios);
+
+		fseek(arxeio, -3*int_size, SEEK_CUR);
+	}
+	printf("\n");
+	
+	fclose(arxeio);
+    system("pause"); 
+}
